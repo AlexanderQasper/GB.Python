137996ae37a37517e24ad1f84e76006af7576fb6
Alex QAsper <semidog@gmail.com>
2022-09-03 16:53:03 +0300

    solved  3 task without understanding

--- Seminar_1.py
+++ Seminar_1.py
@@ -30,8 +30,27 @@
 # print(f"Номер четверти на координатной плоскости: {place_dekart(x, y)}")
 
 # 3. Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
-# ?????
-
+# def inputNumbers(x):
+#     xyz = ["X", "Y", "Z"]
+#     a = []
+#     for i in range(x):
+#         a.append(input(f"Введите значение {xyz[i]}: "))
+#     return a
+#
+#
+# def checkPredicate(x):
+#     left = not (x[0] or x[1] or x[2])
+#     right = not x[0] and not x[1] and not x[2]
+#     result = left == right
+#     return result
+#
+#
+# statement = inputNumbers(3)
+#
+# if checkPredicate(statement) == True:
+#     print(f"Утверждение истинно")
+# else:
+#     print(f"Утверждение ложно")
 
 # 4. Напишите программу, которая по заданному номеру четверти,
 # показывает диапазон возможных координат точек в этой четверти (x и y).
