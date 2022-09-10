c006a620a46c62c2e56a564741119511222c8551
Alex QAsper <semidog@gmail.com>
2022-09-10 10:30:25 +0300

    pizza!

--- Seminar_3.py
+++ Seminar_3.py
@@ -13,22 +13,26 @@
 
 # Заморочился через функцию...
 
-import math
+# import math
+#
+# array = [int(i) for i in input('Введите числа массива через пробел: ').split()]
+#
+#
+# def miltiple_arrays_index(start_array):
+#     end_array = []
+#     if len(start_array) % 2 == 0:
+#         for i in range(1, (len(start_array) // 2) + 1):
+#             end_array.append(start_array[i - 1] * start_array[-i])
+#         return end_array
+#     else:
+#         for i in range(1, (len(start_array) // 2) + 1):
+#             end_array.append(start_array[i - 1] * start_array[-i])
+#         end_array.append((start_array[math.floor(len(start_array) / 2)]) ** 2)
+#         return end_array
+#
+#
+# print(miltiple_arrays_index(array))
 
-array = [int(i) for i in input('Введите числа массива через пробел: ').split()]
+# 3. Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между
+# максимальным и минимальным значением дробной части элементов.
 
-
-def miltiple_arrays_index(start_array):
-    end_array = []
-    if len(start_array) % 2 == 0:
-        for i in range(1, (len(start_array) // 2) + 1):
-            end_array.append(start_array[i - 1] * start_array[-i])
-        return end_array
-    else:
-        for i in range(1, (len(start_array) // 2) + 1):
-            end_array.append(start_array[i - 1] * start_array[-i])
-        end_array.append((start_array[math.floor(len(start_array) / 2)]) ** 2)
-        return end_array
-
-
-print(miltiple_arrays_index(array))
