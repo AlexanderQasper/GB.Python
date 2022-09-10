a37ce41f648ff5f03295c8ac5b866893014a1d24
Alex QAsper <semidog@gmail.com>
2022-09-10 10:25:35 +0300

    solve 2 task

--- Seminar_3.py
+++ Seminar_3.py
@@ -10,3 +10,25 @@
 
 # 2. Напишите программу, которая найдёт произведение пар чисел списка.
 # Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+# Заморочился через функцию...
+
+import math
+
+array = [int(i) for i in input('Введите числа массива через пробел: ').split()]
+
+
+def miltiple_arrays_index(start_array):
+    end_array = []
+    if len(start_array) % 2 == 0:
+        for i in range(1, (len(start_array) // 2) + 1):
+            end_array.append(start_array[i - 1] * start_array[-i])
+        return end_array
+    else:
+        for i in range(1, (len(start_array) // 2) + 1):
+            end_array.append(start_array[i - 1] * start_array[-i])
+        end_array.append((start_array[math.floor(len(start_array) / 2)]) ** 2)
+        return end_array
+
+
+print(miltiple_arrays_index(array))
