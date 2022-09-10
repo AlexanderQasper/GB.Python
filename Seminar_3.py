3845cb8386f93bf34dd523e7cad950941ca353fa
Alex QAsper <semidog@gmail.com>
2022-09-10 11:27:32 +0300

    3 seminar done

--- Seminar_3.py
+++ Seminar_3.py
@@ -59,3 +59,22 @@
 #     binar += str(num % 2)
 #     num //= 2
 # print(binar)
+
+# 5. Cоставьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+
+k = int(input('Введите число: ')) + 1
+
+def get_fibonacci(n):
+    fibo_nums = []
+    a, b = 1, 1
+    for i in range(n-1):
+        fibo_nums.append(a)
+        a, b = b, a + b
+    a, b = 0, 1
+    for i in range(n):
+        fibo_nums.insert(0, a)
+        a, b = b, a - b
+    return fibo_nums
+
+fibo_nums = get_fibonacci(k)
+print(get_fibonacci(k))
