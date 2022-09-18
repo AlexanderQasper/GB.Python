4cb24d6f9b91bf8a3e72079977464c8617afafc9
Alex QAsper <semidog@gmail.com>
2022-09-18 17:30:06 +0300

    new files for RLE algorithm

--- Seminar_5.py
+++ Seminar_5.py
@@ -10,7 +10,7 @@
 # Все конфеты оппонента достаются сделавшему последний ход.
 
 # 2.1. Человек против человека
-from random import randint
+# from random import randint
 #
 # def input_dat(name):
 #     x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
@@ -20,7 +20,6 @@ from random import randint
 #
 # def p_print(name, k, counter, value):
 #     print(f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет.")
-#
 #
 # player1 = input("Введите имя первого игрока: ")
 # player2 = input("Введите имя второго игрока: ")
@@ -55,53 +54,54 @@ from random import randint
 
 
 # 2.2. Добавьте игру против бота
-def input_dat(name):
-    x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
-    while x < 1 or x > 28:
-        x = int(input(f"{name}, введите корректное количество конфет: "))
-    return x
-
-
-def p_print(name, k, counter, value):
-    print(f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет.")
-
-
-def bot_calc(value):
-    k = randint(1,29)
-    while value-k <= 28 and value > 29:
-        k = randint(1,29)
-    return k
-
-player1 = input("Введите имя первого игрока: ")
-player2 = "Bot"
-value = int(input("Введите количество конфет на столе: "))
-flag = randint(0,2) # флаг очередности
-if flag:
-    print(f"Первый ходит {player1}")
-else:
-    print(f"Первый ходит {player2}")
-
-counter1 = 0
-counter2 = 0
-
-while value > 28:
-    if flag:
-        k = input_dat(player1)
-        counter1 += k
-        value -= k
-        flag = False
-        p_print(player1, k, counter1, value)
-    else:
-        k = bot_calc(value)
-        counter2 += k
-        value -= k
-        flag = True
-        p_print(player2, k, counter2, value)
-
-if flag:
-    print(f"Выиграл {player1}")
-else:
-    print(f"Выиграл {player2}")
+# from random import randint
+# def input_dat(name):
+#     x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
+#     while x < 1 or x > 28:
+#         x = int(input(f"{name}, введите корректное количество конфет: "))
+#     return x
+#
+#
+# def p_print(name, k, counter, value):
+#     print(f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет.")
+#
+#
+# def bot_calc(value):
+#     k = randint(1,29)
+#     while value-k <= 28 and value > 29:
+#         k = randint(1,29)
+#     return k
+#
+# player1 = input("Введите имя первого игрока: ")
+# player2 = "Bot"
+# value = int(input("Введите количество конфет на столе: "))
+# flag = randint(0,2) # флаг очередности
+# if flag:
+#     print(f"Первый ходит {player1}")
+# else:
+#     print(f"Первый ходит {player2}")
+#
+# counter1 = 0
+# counter2 = 0
+#
+# while value > 28:
+#     if flag:
+#         k = input_dat(player1)
+#         counter1 += k
+#         value -= k
+#         flag = False
+#         p_print(player1, k, counter1, value)
+#     else:
+#         k = bot_calc(value)
+#         counter2 += k
+#         value -= k
+#         flag = True
+#         p_print(player2, k, counter2, value)
+#
+# if flag:
+#     print(f"Выиграл {player1}")
+# else:
+#     print(f"Выиграл {player2}")
 
 # 3. Tic- Tac
 # import pygame as pg
@@ -197,3 +197,38 @@ else:
 #             screen.blit(text1, [text_x, text_y])
 #
 #     pg.display.update()
+
+# 4. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+
+def rle_encode(data):
+    encoding = ''
+    prev_char = ''
+    count = 1
+
+    if not data: return ''
+    for char in data:
+        if char != prev_char:
+            if prev_char:
+                encoding += str(count) + prev_char
+            count = 1
+            prev_char = char
+        else:
+            count += 1
+    else:
+        encoding += str(count) + prev_char
+        return encoding
+
+def rle_decode(data):
+    decode = ''
+    count = ''
+    for char in data: # If the character is numerical...
+        if char.isdigit():
+            count += char
+        else:
+            decode += char * int(count)
+            count = ''
+    return decode
+
+encoded_val = rle_encode('AAAAAAFDDCCCCCCCAEEEEEEEEEEEEEEEEE')
+print(encoded_val)
+print(rle_decode(encoded_val))
