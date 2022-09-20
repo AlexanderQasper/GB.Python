468194fe7a819e0363d15f36e918110b2bb27c19
Amattavadeva <semidog@gmail.com>
2022-09-20 17:52:10 +0300

    start

--- Seminar_6.py
+++ Seminar_6.py
@@ -1,1 +1,13 @@
-mass = input().split()
+def calc_1(stroka) -> list:
+    res = []
+    n = 0
+    for i in range(len(stroka)):
+        if stroka[i] in ('+', '-', '*', '/'):
+            res.append(stroka[n:i])
+            res.append(stroka[i])
+            n = i + 1
+    res.append(int(stroka[i:]))
+    return res
+
+print(calc_1('5 * 6 / 7'))
+
