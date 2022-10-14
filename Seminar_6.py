40777aba569d1d0f3f681e06ae7bd29d8e550d43
Alex QAsper <semidog@gmail.com>
2022-10-14 14:43:44 +0500

    2 task done

--- Seminar_6.py
+++ Seminar_6.py
@@ -9,18 +9,18 @@
 #     number //= 10
 # print(sum)
 #
-# # После
+# # ПОСЛЕ
 # print(*[sum(int(i) for i in input() if i.isnumeric())])
 
 # 2. Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка,
 # стоящих на нечётной позиции.
 #
 # ДО
-array = [2, 13, 105, 4, 12, 7, 54, 101, 205, 1, 2]
-summa = 0
-for i in range(1, len(array), 2):
-    summa += array[i]
-print(summa)
-
-# После
-print(sum(v for i, v in enumerate(array) if i % 2))
+# array = [2, 13, 105, 4, 12, 7, 54, 101, 205, 1, 2]
+# summa = 0
+# for i in range(1, len(array), 2):
+#     summa += array[i]
+# print(summa)
+#
+# # ПОСЛЕ
+# print(sum(v for i, v in enumerate(array) if i % 2))
