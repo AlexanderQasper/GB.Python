f922b3473b672f254687f9d43ce8022ad96710bf
Alex QAsper <semidog@gmail.com>
2022-10-14 14:20:10 +0500

    2 task done

--- Seminar_6.py
+++ Seminar_6.py
@@ -1,4 +1,28 @@
-orig_text = 'марат карат забвение квартал абдоминальный абвер'
-text1 = orig_text.split()
+# 1. Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
 
-print(*filter(lambda x: 'абв' not in x, text1))
+# ДО
+# number = int(input())
+# sum = 0
+# while number > 0:
+#     digit = number % 10
+#     sum += digit
+#     number //= 10
+# print(sum)
+#
+# # После
+# print(*[sum(int(i) for i in input() if i.isnumeric())])
+
+# 2. Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка,
+# стоящих на нечётной позиции.
+#
+# ДО
+array = [2, 13, 105, 4, 12, 7, 54, 101, 205, 1, 2]
+summa = 0
+for i in range(1, len(array), 2):
+    summa += array[i]
+print(summa)
+
+# После
+print(sum(v for i, v in enumerate(array) if i % 2))
+
+#
