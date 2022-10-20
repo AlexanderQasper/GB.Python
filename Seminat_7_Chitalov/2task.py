5f8375ed7fe73927ba6c293a132c6e737155687e
Alex QAsper <semidog@gmail.com>
2022-10-20 17:31:55 +0500

    2 task solved

--- Seminat_7_Chitalov/2task.py
+++ Seminat_7_Chitalov/2task.py
@@ -1,6 +1,19 @@
 class Road:
 
-    _lenght = 0
-    _width = 0
+    __lenght = None
+    __width = None
+    weight = None
+    thickness = None
+    def __init__ (self, length, width):
+        self.lenght = length
+        self.width = width
+        print('Данные получены')
 
-def Calculate_r
+    def calculate(self):
+        self.weight = 25
+        self.thickness = 0.05
+        calc = self.lenght * self.width * self.weight * self.thickness / 1000
+        print(f'Необходимо {calc} тонн для строительства')
+
+my_road = Road(20000, 6)
+my_road.calculate()
