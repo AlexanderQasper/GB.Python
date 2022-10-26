2c6ec76adf24aa7575e618e973b8b364fa067768
Alex QAsper <semidog@gmail.com>
2022-10-26 18:08:41 +0500

    seminar 8 2 task add discriptions

--- Seminar_8.py
+++ Seminar_8.py
@@ -1,39 +1,85 @@
 # 1. Реализовать класс Matrix (матрица). Обеспечить перегрузку конструктора класса (метод __init__()),
 # который должен принимать данные (список списков) для формирования матрицы.
-import copy
+# import copy
+#
+#
+# class Matrix:
+#
+#     # Перегружаем метод инициализации в конструктор класса. Если правильно понял, класс матрица при вызове сразу
+#     # получают сущнность мартицы
+#     def __init__(self, matrix):
+#         self.matrix = matrix
+#
+#     # Вмешиваемся в принты, создаём пустую строку и добавляем циклом в неё числа матрицы, переводя их в строку и
+#     # используем метод джоин + переход на новую строку для красоты
+#     def __str__(self):
+#         s = ''
+#         for i in range(len(self.matrix)):
+#             s = s + '\t'.join(map(str, self.matrix[i])) + '\n'
+#         return s
+#
+#     # Перехватываем встроенный функционал сложения, прописывая, что и как нужно складывать. Вложенным циклом, как обычныую функцию сложения матрицы
+#     def __add__(self, other):
+#         if len(self.matrix) != len(other.matrix):
+#             return None
+#         res = copy.deepcopy(self.matrix)
+#         for i in range(len(self.matrix)):
+#             for k in range(len(self.matrix[i])):
+#                 res[i][k] = self.matrix[i][k] + other.matrix[i][k]
+#         return Matrix(res)
+#
+#
+# l1 = [[1, 2, 4], [3, 4, 5], [5, 6, 6]]
+# l2 = [[11, 21, 41], [31, 41, 51], [51, 61, 61]]
+# matrix1 = Matrix(l1)
+# matrix2 = Matrix(l2)
+# print(matrix1)
+# print(matrix2)
+# end_matrix = matrix1 + matrix2
+# print(end_matrix)
+
+# 2 Реализовать проект расчета суммарного расхода ткани на производство одежды.
+# Основная сущность (класс) этого проекта — одежда, которая может иметь определенное название.
+# К типам одежды в этом проекте относятся пальто и костюм. У этих типов одежды существуют параметры:
+# размер (для пальто) и рост (для костюма). Это могут быть обычные числа: V и H, соответственно.
+# Для определения расхода ткани по каждому типу одежды использовать формулы: для пальто (V/6.5 + 0.5),
+# для костюма (2 * H + 0.3). Проверить работу этих методов на реальных данных.
+# Реализовать общий подсчет расхода ткани. Проверить на практике полученные на этом уроке знания:
+# реализовать абстрактные классы для основных классов проекта, проверить на практике работу декоратора @property.
+
+from abc import ABC, abstractmethod
 
 
-class Matrix:
+class Clothes(ABC):
+    # При инициализации говорим, что сразу, при вызове будем передавать параметр
+    def __init__(self, param):
+        self.param = param
 
-    # Перегружаем метод инициализации в конструктор класса. Если правильно понял, класс матрица при вызове сразу
-    # получают сущнность мартицы
-    def __init__(self, matrix):
-        self.matrix = matrix
+    # Декоратором будем проверять, что дальше ОБЯЗАТЕЛЬНО этот метод будет изменён
+    @abstractmethod
+    def expense(self):
+        pass
 
-    # Вмешиваемся в принты, создаём пустую строку и добавляем циклом в неё числа матрицы, переводя их в строку и
-    # используем метод джоин + переход на новую строку для красоты
     def __str__(self):
-        s = ''
-        for i in range(len(self.matrix)):
-            s = s + '\t'.join(map(str, self.matrix[i])) + '\n'
-        return s
-
-    # Перехватываем встроенный функционал сложения, прописывая, что и как нужно складывать. Вложенным циклом, как обычныую функцию сложения матрицы
-    def __add__(self, other):
-        if len(self.matrix) != len(other.matrix):
-            return None
-        res = copy.deepcopy(self.matrix)
-        for i in range(len(self.matrix)):
-            for k in range(len(self.matrix[i])):
-                res[i][k] = self.matrix[i][k] + other.matrix[i][k]
-        return Matrix(res)
+        return str(self.param)
 
 
-l1 = [[1, 2, 4], [3, 4, 5], [5, 6, 6]]
-l2 = [[11, 21, 41], [31, 41, 51], [51, 61, 61]]
-matrix1 = Matrix(l1)
-matrix2 = Matrix(l2)
-print(matrix1)
-print(matrix2)
-end_matrix = matrix1 + matrix2
-print(end_matrix)
+class Coat(Clothes):
+
+    @property
+    def expense(self):
+        return self.param / 6.5 + 0.5
+
+
+class Suit(Clothes):
+
+    @property
+    def expense(self):
+        return self.param * 2 + 0.3
+
+
+a = Coat(52)
+b = Suit(1.80)
+print(a)
+print(a.expense)
+print(b.expense)
