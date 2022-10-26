e5a298e2a23a1c9c265e78e5ddae390d1675170b
Alex QAsper <semidog@gmail.com>
2022-10-26 18:11:22 +0500

    seminar 8 3 task done

--- Seminar_8.py
+++ Seminar_8.py
@@ -47,39 +47,106 @@
 # Реализовать общий подсчет расхода ткани. Проверить на практике полученные на этом уроке знания:
 # реализовать абстрактные классы для основных классов проекта, проверить на практике работу декоратора @property.
 
-from abc import ABC, abstractmethod
+# from abc import ABC, abstractmethod
+#
+#
+# class Clothes(ABC):
+#     # При инициализации говорим, что сразу, при вызове будем передавать параметр
+#     def __init__(self, param):
+#         self.param = param
+#
+#     # Декоратором будем проверять, что дальше ОБЯЗАТЕЛЬНО этот метод будет изменён
+#     @abstractmethod
+#     def expense(self):
+#         pass
+#
+#     def __str__(self):
+#         return str(self.param)
+#
+#
+# class Coat(Clothes):
+#
+#     @property
+#     def expense(self):
+#         return self.param / 6.5 + 0.5
+#
+#
+# class Suit(Clothes):
+#
+#     @property
+#     def expense(self):
+#         return self.param * 2 + 0.3
+#
+#
+# a = Coat(52)
+# b = Suit(1.80)
+# print(a)
+# print(a.expense)
+# print(b.expense)
 
+# Задание 3.
+# Реализовать программу работы с органическими клетками, состоящими из ячеек.
+# Необходимо создать класс Клетка (Cell).
+# В его конструкторе инициализировать параметр (quantity),
+# соответствующий количеству ячеек клетки (целое число).
+# В классе должны быть реализованы методы перегрузки арифметических операторов:
+# сложение (add()),
+# вычитание (sub()),
+# умножение (mul()),
+# деление (truediv()).
+# Данные методы должны применяться только к клеткам и выполнять увеличение,
+# уменьшение, умножение и целочисленное (с округлением до целого) деление клеток, соответственно.
+# Сложение. Объединение двух клеток.
+# При этом число ячеек общей клетки должно равняться сумме ячеек исходных двух клеток.
+# Вычитание. Участвуют две клетки.
+# Операцию необходимо выполнять только если разность количества ячеек двух клеток больше нуля,
+# иначе выводить соответствующее сообщение.
+# Умножение. Создается общая клетка из двух.
+# Число ячеек общей клетки определяется как произведение количества ячеек этих двух клеток.
+# Деление. Создается общая клетка из двух.
+# Число ячеек общей клетки определяется как целочисленное деление количества ячеек этих двух клеток.
 
-class Clothes(ABC):
-    # При инициализации говорим, что сразу, при вызове будем передавать параметр
-    def __init__(self, param):
-        self.param = param
-
-    # Декоратором будем проверять, что дальше ОБЯЗАТЕЛЬНО этот метод будет изменён
-    @abstractmethod
-    def expense(self):
-        pass
+class Cell:
+    def __init__(self, cell):
+        self.cell = cell
+        self.simbol = '*'
 
     def __str__(self):
-        return str(self.param)
+        return str(f'Количество ячеек - {self.cell}')
+
+    def __sub__(self, other):
+        return Cell(abs(self.cell - other.cell))
+
+    def __mul__(self, other):
+        return Cell(self.cell * other.cell)
+
+    def __truediv__(self, other):
+        return Cell(self.cell // other.cell)
+
+    def __add__(self, other):
+        return Cell(abs(self.cell + other.cell))
+
+    def make_order(self, count):
+        x = self.cell
+        while x > 0:
+            for k in range(1,count+1):
+                print(self.simbol, end ='')
+                x -= 1
+                if x <= 0:
+                    break
+            print('\n', end = '')
 
 
-class Coat(Clothes):
 
-    @property
-    def expense(self):
-        return self.param / 6.5 + 0.5
+a = Cell(5)
+b = Cell(10)
+c = Cell(3)
+d = Cell(2)
 
+print(a + b)
+print(a - b)
+print(a * b)
+print(c / d)
 
-class Suit(Clothes):
-
-    @property
-    def expense(self):
-        return self.param * 2 + 0.3
-
-
-a = Coat(52)
-b = Suit(1.80)
-print(a)
-print(a.expense)
-print(b.expense)
+a.make_order(3)
+b.make_order(3)
