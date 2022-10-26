bf8356a2eab69aff880fc2fe060e4a6c474d55aa
Alex QAsper <semidog@gmail.com>
2022-10-26 21:15:44 +0500

    use equal, NotEqual, True

--- Seminar_9.Unit_tests/Functions.py
+++ Seminar_9.Unit_tests/Functions.py
@@ -11,4 +11,43 @@ def distance_point(a, b, c, d):
     return round(distance, 2)
 
 
-print(distance_point(2, 3, 2, 8))
+# 2. Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+# номер четверти плоскости, в которой находится эта точка (или на какой оси она находится).
+
+
+def place_dekart(x, y):
+    try:
+        if x > 0 and y > 0:
+            return 1
+        elif x < 0 and y > 0:
+            return 2
+        elif x < 0 and y < 0:
+            return 3
+        else:
+            return 4
+    except:
+        return 'Ошибка!'
+
+
+#2. Напишите программу, которая найдёт произведение пар чисел списка.
+#Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+
+array = [1, 2, 3, 4, 5, 6]
+
+
+def multiple_arrays_index(start_array):
+    end_array = []
+    if len(start_array) % 2 == 0:
+        for i in range(1, (len(start_array) // 2) + 1):
+            end_array.append(start_array[i - 1] * start_array[-i])
+        return end_array
+    else:
+        for i in range(1, (len(start_array) // 2) + 1):
+            end_array.append(start_array[i - 1] * start_array[-i])
+        end_array.append((start_array[floor(len(start_array) / 2)]) ** 2)
+        return end_array
+
+
+print(type(multiple_arrays_index(array)) == list)
+print(multiple_arrays_index(array))
--- Seminar_9.Unit_tests/Unit_test.py
+++ Seminar_9.Unit_tests/Unit_test.py
@@ -1,7 +1,24 @@
 import unittest
 from Functions import *
 
-class Testcases(unittest.TestCase):
+
+class Testcases1(unittest.TestCase):
 
     def test_distance_point(self):
         self.assertEqual(distance_point(2, 3, 2, 8), 5.0)
+
+    def test_place_dekart1(self):
+        self.assertNotEqual(place_dekart(1, 2), 3)
+
+# Этот, почему-то проходит всегда ОК, хотя вроде должен обрабатывать эксепшен (прописан в функции специально)
+    def test_place_dekart2(self):
+        self.assertRaises(Exception, place_dekart('3', 3))
+
+    def test_miltiple_arrays_index(self):
+        self.assertTrue(type(multiple_arrays_index(array)) == list)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
