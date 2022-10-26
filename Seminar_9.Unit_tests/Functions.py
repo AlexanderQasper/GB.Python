e148b7c0471f8402d7590aefd01b1a45acef19de
Alex QAsper <semidog@gmail.com>
2022-10-26 22:04:04 +0500

    use assertIn, assertFalse

--- Seminar_9.Unit_tests/Functions.py
+++ Seminar_9.Unit_tests/Functions.py
@@ -1,7 +1,8 @@
 from math import *
 
 
-# 1. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+# 1. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D
+# пространстве.
 
 
 def distance_point(a, b, c, d):
@@ -26,8 +27,7 @@ def place_dekart(x, y):
         return 4
 
 
-
-# 2. Напишите программу, которая найдёт произведение пар чисел списка.
+# 3. Напишите программу, которая найдёт произведение пар чисел списка.
 # Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 
 def multiple_arrays_index(start_array):
@@ -41,3 +41,26 @@ def multiple_arrays_index(start_array):
             end_array.append(start_array[i - 1] * start_array[-i])
         end_array.append((start_array[floor(len(start_array) / 2)]) ** 2)
         return end_array
+
+
+# 4. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+def into_binar(x):
+    n = ""
+    while x > 0:
+        y = str(x % 2)
+        n = y + n
+        x //= 2
+    return n
+
+
+# 5. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+def multipliers_num(x):
+    number = x
+    multipliers = []
+
+    for i in range(1, number // 2 + 1):
+        if number % i == 0:
+            multipliers.append(i)
+    return multipliers
--- Seminar_9.Unit_tests/Unit_test.py
+++ Seminar_9.Unit_tests/Unit_test.py
@@ -2,21 +2,39 @@ import unittest
 from Functions import *
 
 
-class Testcases1(unittest.TestCase):
+class Testcases(unittest.TestCase):
 
+    # Equal
     def test_distance_point(self):
         self.assertEqual(distance_point(2, 3, 2, 8), 5.0)
 
+    # NotEqual
     def test_place_dekart1(self):
         self.assertNotEqual(place_dekart(1, 2), 3)
 
-    # Этот, почему-то проходит всегда ОК, хотя вроде должен обрабатывать эксепшен (прописан в функции специально)
-    # Этот момент непонятен, гуглёж только
+    # TypeError
     def test_place_dekart2(self):
         self.assertRaises(TypeError, place_dekart, '4', 3)
 
+    # True
     def test_miltiple_arrays_index(self):
         self.assertTrue(type(multiple_arrays_index([1, 2, 3, 4, 5, 6])) == list)
+
+    # false
+    def test_into_binar(self):
+        self.assertFalse(type(into_binar(8)) == float)
+
+    # in
+    def test_multipliers_num(self):
+        self.assertIn(2, multipliers_num(10))
+
+    # Is
+
+    # isnot
+
+    # IsNone
+
+    # IsNotNone
 
 
 if __name__ == '__main__':
